f57991f2583afbb83e90edfa06be980ac63d2f20
basketbwall <55670468+basketbwall@users.noreply.github.com>
2021-02-20 18:36:50 -0500

    Add files via upload

---
+++ RubricSearch/RubricSearch/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace RubricSearch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblKeyWord = new System.Windows.Forms.Label();
+            this.txtKeyWord = new System.Windows.Forms.TextBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.lstbFolder = new System.Windows.Forms.ListBox();
+            this.txtDirectory = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblKeyWord
+            // 
+            this.lblKeyWord.AutoSize = true;
+            this.lblKeyWord.Location = new System.Drawing.Point(12, 22);
+            this.lblKeyWord.Name = "lblKeyWord";
+            this.lblKeyWord.Size = new System.Drawing.Size(149, 13);
+            this.lblKeyWord.TabIndex = 0;
+            this.lblKeyWord.Text = "Enter a key word to search for";
+            // 
+            // txtKeyWord
+            // 
+            this.txtKeyWord.Location = new System.Drawing.Point(167, 19);
+            this.txtKeyWord.Name = "txtKeyWord";
+            this.txtKeyWord.Size = new System.Drawing.Size(100, 20);
+            this.txtKeyWord.TabIndex = 1;
+            this.txtKeyWord.TextChanged += new System.EventHandler(this.txtKeyWord_TextChanged);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(500, 456);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(15, 212);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ReadOnly = true;
+            this.txtOutput.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtOutput.Size = new System.Drawing.Size(560, 238);
+            this.txtOutput.TabIndex = 4;
+            // 
+            // lstbFolder
+            // 
+            this.lstbFolder.FormattingEnabled = true;
+            this.lstbFolder.Location = new System.Drawing.Point(15, 126);
+            this.lstbFolder.Name = "lstbFolder";
+            this.lstbFolder.Size = new System.Drawing.Size(560, 69);
+            this.lstbFolder.TabIndex = 5;
+            this.lstbFolder.SelectedIndexChanged += new System.EventHandler(this.lstbFolder_SelectedIndexChanged);
+            // 
+            // txtDirectory
+            // 
+            this.txtDirectory.Location = new System.Drawing.Point(15, 92);
+            this.txtDirectory.Name = "txtDirectory";
+            this.txtDirectory.Size = new System.Drawing.Size(146, 23);
+            this.txtDirectory.TabIndex = 6;
+            this.txtDirectory.Text = "Add Directory";
+            this.txtDirectory.UseVisualStyleBackColor = true;
+            this.txtDirectory.Click += new System.EventHandler(this.txtDirectory_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(587, 491);
+            this.Controls.Add(this.txtDirectory);
+            this.Controls.Add(this.lstbFolder);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.txtKeyWord);
+            this.Controls.Add(this.lblKeyWord);
+            this.Name = "Form1";
+            this.Text = "SyllabusSearch";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblKeyWord;
+        private System.Windows.Forms.TextBox txtKeyWord;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.ListBox lstbFolder;
+        private System.Windows.Forms.Button txtDirectory;
+    }
+}
+
---
+++ RubricSearch/RubricSearch/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.IO;
+using System.Collections;
+
+namespace RubricSearch
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void txtDirectory_Click(object sender, EventArgs e) //user clicks add directory button to trigger this function
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog(); //menu pops up to allow user to choose a directory
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                lstbFolder.Items.Clear();
+                string[] files = Directory.GetFiles(fbd.SelectedPath); //get every file in the chosen directory
+
+                foreach (string file in files)
+                {
+                    if (file.EndsWith(".txt")) //we are only interested in txt files for now, maybe modify this for pdfs/word docs?
+                    {
+                        lstbFolder.Items.Add(file);
+                    }
+                }
+            }
+        }
+
+        private void lstbFolder_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string keyWord = txtKeyWord.Text; //currently this program only checks for 1 key word across each document
+            string filePath = lstbFolder.Items[lstbFolder.SelectedIndex].ToString();
+            ArrayList lines = new ArrayList();
+            int keyWordCount = 0;
+            StreamReader textIn = new StreamReader(new FileStream(filePath, FileMode.OpenOrCreate, FileAccess.Read));
+            while (textIn.Peek() != -1) //keep searching until there's nothing in the file left to read
+            {
+                string row = textIn.ReadLine(); //we'll read line by line
+                if (row.Contains(keyWord)) //if the current line contains out keyword update some statistics like keyWordCount and save that line
+                {
+                    lines.Add(row.ToString() + Environment.NewLine);
+                    keyWordCount++;
+                }
+
+
+            }
+            string linesOutput = "";
+            foreach (string item in lines) // ArrayList tostring doesn't give us meaningful data, so we're looping through the arraylist manually to grab the lines
+            {
+                linesOutput += Environment.NewLine + item.ToString();
+            }
+            txtOutput.Text = keyWord + " occurred in " + keyWordCount.ToString() + " part(s) of the document" + Environment.NewLine +
+                linesOutput;
+        }
+
+        private void txtKeyWord_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
