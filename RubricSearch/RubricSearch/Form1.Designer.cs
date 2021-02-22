f98559f5b89a9eeecb76d0b22824f64419532ed0
basketbwall <55670468+basketbwall@users.noreply.github.com>
2021-02-21 21:32:24 -0500

    Add files via upload
    Added functionality to search for multiple key words and other miscellaneous features for ease of use such as buttons to change font size and setting window to always be on top.

--- RubricSearch/RubricSearch/Form1.Designer.cs
+++ RubricSearch/RubricSearch/Form1.Designer.cs
@@ -34,16 +34,20 @@
             this.txtOutput = new System.Windows.Forms.TextBox();
             this.lstbFolder = new System.Windows.Forms.ListBox();
             this.txtDirectory = new System.Windows.Forms.Button();
+            this.txtFontBigger = new System.Windows.Forms.Button();
+            this.txtFontSmaller = new System.Windows.Forms.Button();
+            this.cbAlwaysOnTop = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // lblKeyWord
             // 
-            this.lblKeyWord.AutoSize = true;
             this.lblKeyWord.Location = new System.Drawing.Point(12, 22);
             this.lblKeyWord.Name = "lblKeyWord";
-            this.lblKeyWord.Size = new System.Drawing.Size(149, 13);
+            this.lblKeyWord.Size = new System.Drawing.Size(149, 57);
             this.lblKeyWord.TabIndex = 0;
-            this.lblKeyWord.Text = "Enter a key word to search for";
+            this.lblKeyWord.Text = "Enter a key word to search for\r\n\r\nFor multiple keywords, use a comma to separate " +
+    "them";
+            this.lblKeyWord.Click += new System.EventHandler(this.lblKeyWord_Click);
             // 
             // txtKeyWord
             // 
@@ -88,21 +92,56 @@
             this.txtDirectory.Name = "txtDirectory";
             this.txtDirectory.Size = new System.Drawing.Size(146, 23);
             this.txtDirectory.TabIndex = 6;
-            this.txtDirectory.Text = "Add Directory";
+            this.txtDirectory.Text = "Add Directory of the files";
             this.txtDirectory.UseVisualStyleBackColor = true;
             this.txtDirectory.Click += new System.EventHandler(this.txtDirectory_Click);
+            // 
+            // txtFontBigger
+            // 
+            this.txtFontBigger.Location = new System.Drawing.Point(96, 456);
+            this.txtFontBigger.Name = "txtFontBigger";
+            this.txtFontBigger.Size = new System.Drawing.Size(75, 23);
+            this.txtFontBigger.TabIndex = 7;
+            this.txtFontBigger.Text = "Bigger Font";
+            this.txtFontBigger.UseVisualStyleBackColor = true;
+            this.txtFontBigger.Click += new System.EventHandler(this.txtFontBigger_Click);
+            // 
+            // txtFontSmaller
+            // 
+            this.txtFontSmaller.Location = new System.Drawing.Point(15, 456);
+            this.txtFontSmaller.Name = "txtFontSmaller";
+            this.txtFontSmaller.Size = new System.Drawing.Size(75, 23);
+            this.txtFontSmaller.TabIndex = 8;
+            this.txtFontSmaller.Text = "Smaller Font";
+            this.txtFontSmaller.UseVisualStyleBackColor = true;
+            this.txtFontSmaller.Click += new System.EventHandler(this.txtFontSmaller_Click);
+            // 
+            // cbAlwaysOnTop
+            // 
+            this.cbAlwaysOnTop.AutoSize = true;
+            this.cbAlwaysOnTop.Location = new System.Drawing.Point(477, 22);
+            this.cbAlwaysOnTop.Name = "cbAlwaysOnTop";
+            this.cbAlwaysOnTop.Size = new System.Drawing.Size(98, 17);
+            this.cbAlwaysOnTop.TabIndex = 9;
+            this.cbAlwaysOnTop.Text = "Always On Top";
+            this.cbAlwaysOnTop.UseVisualStyleBackColor = true;
+            this.cbAlwaysOnTop.CheckedChanged += new System.EventHandler(this.cbAlwaysOnTop_CheckedChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(587, 491);
+            this.Controls.Add(this.cbAlwaysOnTop);
+            this.Controls.Add(this.txtFontSmaller);
+            this.Controls.Add(this.txtFontBigger);
             this.Controls.Add(this.txtDirectory);
             this.Controls.Add(this.lstbFolder);
             this.Controls.Add(this.txtOutput);
             this.Controls.Add(this.btnExit);
             this.Controls.Add(this.txtKeyWord);
             this.Controls.Add(this.lblKeyWord);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "Form1";
             this.Text = "SyllabusSearch";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -119,6 +158,9 @@
         private System.Windows.Forms.TextBox txtOutput;
         private System.Windows.Forms.ListBox lstbFolder;
         private System.Windows.Forms.Button txtDirectory;
+        private System.Windows.Forms.Button txtFontBigger;
+        private System.Windows.Forms.Button txtFontSmaller;
+        private System.Windows.Forms.CheckBox cbAlwaysOnTop;
     }
 }
 
--- RubricSearch/RubricSearch/Form1.cs
+++ RubricSearch/RubricSearch/Form1.cs
@@ -48,36 +48,91 @@ namespace RubricSearch
             }
         }
 
+        private Boolean stringContainsWords(string input, string[] words) //checks if string contains atleast 1 of the words in the array
+        {
+            for(int i=0; i<words.Length; i++)
+            {
+                if (input.Contains(words[i]))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        private Boolean arrayAlreadyContains(ArrayList list, string row)
+        {
+            for(int i=0; i<list.Count; i++)
+            {
+                if (list[i].Equals(row))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
         private void lstbFolder_SelectedIndexChanged(object sender, EventArgs e)
         {
-            string keyWord = txtKeyWord.Text; //currently this program only checks for 1 key word across each document
+            string[] keyWords = txtKeyWord.Text.ToLower().Split(','); //make it all lowercase and split by comma
             string filePath = lstbFolder.Items[lstbFolder.SelectedIndex].ToString();
             ArrayList lines = new ArrayList();
             int keyWordCount = 0;
             StreamReader textIn = new StreamReader(new FileStream(filePath, FileMode.OpenOrCreate, FileAccess.Read));
             while (textIn.Peek() != -1) //keep searching until there's nothing in the file left to read
             {
-                string row = textIn.ReadLine(); //we'll read line by line
-                if (row.Contains(keyWord)) //if the current line contains out keyword update some statistics like keyWordCount and save that line
+                string row = textIn.ReadLine().ToLower(); //we'll read line by line make sure it's all lowercase because we don't care about casing
+                if (stringContainsWords(row, keyWords)) //if the current line contains out keyword update some statistics like keyWordCount and save that line
                 {
-                    lines.Add(row.ToString() + Environment.NewLine);
-                    keyWordCount++;
+                    if (!arrayAlreadyContains(lines, row)) 
+                    {
+                        lines.Add(row.ToString() + Environment.NewLine);
+                        keyWordCount++;
+                    }
+
                 }
 
 
+
             }
+
             string linesOutput = "";
             foreach (string item in lines) // ArrayList tostring doesn't give us meaningful data, so we're looping through the arraylist manually to grab the lines
             {
                 linesOutput += Environment.NewLine + item.ToString();
             }
-            txtOutput.Text = keyWord + " occurred in " + keyWordCount.ToString() + " part(s) of the document" + Environment.NewLine +
+            txtOutput.Text = txtKeyWord.Text + " occurred in " + keyWordCount.ToString() + " part(s) of the document" + Environment.NewLine +
                 linesOutput;
         }
 
         private void txtKeyWord_TextChanged(object sender, EventArgs e)
         {
+            lstbFolder_SelectedIndexChanged(null, null);
+        }
 
+        private void lblKeyWord_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtFontSmaller_Click(object sender, EventArgs e)
+        {
+            float currentSize = txtOutput.Font.Size;
+            currentSize -= 1.0F;
+            txtOutput.Font = new Font(txtOutput.Font.Name, currentSize,
+                txtOutput.Font.Style, txtOutput.Font.Unit);
+        }
+
+        private void txtFontBigger_Click(object sender, EventArgs e)
+        {
+            float currentSize = txtOutput.Font.Size;
+            currentSize += 1.0F;
+            txtOutput.Font = new Font(txtOutput.Font.Name, currentSize,
+                txtOutput.Font.Style, txtOutput.Font.Unit);
+        }
+
+        private void cbAlwaysOnTop_CheckedChanged(object sender, EventArgs e)
+        {
+            this.TopMost = !this.TopMost;
         }
     }
 }
